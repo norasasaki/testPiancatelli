2c15eb03541ab76f16396aff039696a651c66b0a
norasasaki <79462887+norasasaki@users.noreply.github.com>
2023-03-21 20:41:20 +0100

    Add files via upload

--- complexversion/src/counter.js
+++ complexversion/src/counter.js
@@ -13,11 +13,11 @@ const Counter = (props) => {
 
     return (
       <div className = "littleApp" >
-          <h2> Contatore </h2>
-          <div className="Counter" style ={{color: count === 0 ? "black" : "red"}}> {count} </div>
-          <button type="button" id="Increment"  className="btn btn-primary" onClick = { adding } > Increment </button>
-          <button type="button" id="resetto" className="btn btn-danger" onClick = { reset }> Reset </button>
-      </div>
+        <h2> Contatore </h2>
+        <button type="button" id="increment"  className="btn btn-primary" onClick = { adding } > Increment </button>
+        <button type="button" id="reset" className="btn btn-danger" onClick = { reset }> Reset </button>
+		<div className="Counter" >Counter: <h4 style ={{color: count === 0 ? "black" : "red"}}>{count}</h4> </div>
+	  </div>
     );
   }
 
