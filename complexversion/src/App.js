76711b303e283bc117554ec9ada7b85040c16407
norasasaki <79462887+norasasaki@users.noreply.github.com>
2023-03-21 16:41:00 +0100

    Add files via upload

---
+++ complexversion/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './counter.css';
+import Counter from './counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ complexversion/src/counter.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+
+const Counter = (props) => {
+   let [count, setCount ] = useState(0);
+   
+   const reset = () =>{
+    setCount(0)
+  }
+  const adding = () =>{
+    setCount(count+1)
+  }
+
+    return (
+      <div className = "littleApp" >
+          <h2> Contatore </h2>
+          <div className="Counter" style ={{color: count === 0 ? "black" : "red"}}> {count} </div>
+          <button type="button" id="Increment"  className="btn btn-primary" onClick = { adding } > Increment </button>
+          <button type="button" id="resetto" className="btn btn-danger" onClick = { reset }> Reset </button>
+      </div>
+    );
+  }
+
+  export default Counter
